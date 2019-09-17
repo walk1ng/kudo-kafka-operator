e80cd2dba720441c777d63a9cff5d85d38a06261
Marcin Owsiany <marcin@owsiany.pl>
2019-09-17 14:38:06 +0200

    Add a comment, set -u.

--- tests/run.sh
+++ tests/run.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
-set -e
+# This script is intended to be run in a docker container by the ../run-tests.sh script, not manually.
+set -eu
 export GO111MODULE=on
 export PATH=${PATH}:/${VENDOR_DIR}
 go mod edit -require github.com/kudobuilder/kudo@${DS_KUDO_VERSION}
 go install github.com/onsi/ginkgo/ginkgo
 ${KUBECTL_PATH} kudo version
 
-if [[ ! -z $TESTS_FOCUS ]]; then
-	ginkgo ./suites/... --ginkgo.focus=${TESTS_FOCUS}
-else
-	ginkgo ./suites/...
-fi
+ginkgo ./suites/... ${TESTS_FOCUS:+--ginkgo.focus=${TESTS_FOCUS}}
